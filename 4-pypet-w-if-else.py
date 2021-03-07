bacb3b3548a4c1436dd5c66191c0c135173fcb09
beewanca <76642696+beewanca@users.noreply.github.com>
2021-03-07 19:58:01 -0300

    Update 4-pypet-w-if-else.py

--- 4-pypet-w-if-else.py
+++ 4-pypet-w-if-else.py
@@ -3,12 +3,6 @@ print("Welcome to Pypet!", "\n" +
 
 input('Press Enter...')
 
-name = 'Pyworm'
-age = 7
-weight = 10.5
-hungry = True
-photo = '~~~~~~~'
-
 worm = {
      'name': 'Pyworm',
      'age': 7,
